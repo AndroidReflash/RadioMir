533075f1348364ef7c7b6f35cede28bdc436678c
Droid <9kartblansh9@gmail.com>
2025-07-14 04:43:40 +0300

    correct url

--- app/src/main/java/com/example/radiomir/Url.kt
+++ app/src/main/java/com/example/radiomir/Url.kt
@@ -2,5 +2,5 @@ package com.example.radiomir
 
 sealed class Url(val value: String){
     //up-to-date
-    data object RadioMir: Url("https://icecast-mirtv.cdnvideo.ru/radio_mir_256")
+    data object RadioMir: Url("https://media1.datacenter.by:1936/radiomir/radiomir/playlist.m3u8")
 }
